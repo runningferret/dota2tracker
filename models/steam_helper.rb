33511587308680723d2f72df9b06ec51b4fdb836
Tony Castiglione <tcastiglione@gmail.com>
2013-10-11 20:14:58 -0500

    Add more methods

--- models/steam_helper.rb
+++ models/steam_helper.rb
@@ -7,16 +7,44 @@ class SteamHelper
     def initialize( api_key )
         WebApi.api_key = api_key
     end
-
-    def get_matches( player_name )
-        WebApi.json( "IDOTA2Match_#{DOTA2_ID}",
-            "GetMatchHistory",
-            1,
-            player_name: escape_spaces( player_name )
-        )
-    end
     
+    def get_most_recent_match( account_id )
+      JSON.parse( get_matches( account_id ))['result']['matches'].first
+    end
+
+    def get_most_recent_hero( account_id )
+      match = get_most_recent_match( account_id )
+      player = match['players'].select{ |p| p['account_id'] == account_id }
+      get_hero_by_id( player.first['hero_id'] )
+
+    end
+
     private
+
+    def get_hero_by_id( id )
+      parsed = JSON.parse get_heroes
+      heroes = {}
+      parsed['result']['heroes'].each do |h|
+        heroes[h['id']] = h['name'].gsub('npc_dota_hero_','').gsub('_', ' ')
+      end
+      
+      heroes[id]
+    end
+
+    def get_matches( account_id )
+      WebApi.json( "IDOTA2Match_#{DOTA2_ID}",
+        "GetMatchHistory",
+        1,
+        account_id: account_id
+      )
+    end
+
+    def get_heroes
+      WebApi.json( "IEconDOTA2_#{DOTA2_ID}",
+        "GetHeroes",
+        1
+      )
+    end
 
     def escape_spaces( string )
         string.gsub!(' ', '%20')
