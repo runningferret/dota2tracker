31a445c376b0f09be9286334c5f7be4bfc4f4d97
Tony Castiglione <tcastiglione@gmail.com>
2013-10-23 18:14:35 -0500

    Respond with valid IVR i hope

--- dota2tracker.rb
+++ dota2tracker.rb
@@ -5,5 +5,17 @@ get '/' do
 end
 
 get '/last_match' do
-  params.to_s
+<<-HERE
+<action>
+<app>SurVo</app>
+<parameters>
+<id>75</id>
+<user_parameters>
+<first_name>Bill</first_name>
+<last_name>Johnson</last_name>
+</user_parameters>
+<p_t></p_t>
+</parameters>
+</action>
+HERE
 end
