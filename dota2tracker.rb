5919b87bc76e4478098ab81534ef4305fa761c02
Tony Castiglione <tcastiglione@gmail.com>
2013-11-09 13:25:36 -0500

    Remove API key! Dumb. Regenerated a new key, pass it in now

--- dota2tracker.rb
+++ dota2tracker.rb
@@ -1,24 +1,12 @@
 require 'sinatra'
 require './models/last_match_helper'
 
+STEAM_API_KEY = 
 get '/' do
   account_id = params['accountid']
-  lmh = LastMatchHelper.new( "DF08597129E71A0EBF7A2FA880A437CD", account_id.to_i )
+  steam_key = params['api_key']
+  return "Specify a steam api key(api_key) and 32 bit id(account_id)!" unless account_id && steam_key
+  lmh = LastMatchHelper.new(steam_key, account_id.to_i )
+  require 'debugger'; debugger
   lmh.get_some_interesting_information
-end
-
-get '/last_match', :provides => ['xml'] do
-<<-HERE
-<action>
-<app>findme</app>
-<parameters>
-<id>708204</id>
-<user_parameters>
-<first_name>Bill</first_name>
-<last_name>Johnson</last_name>
-</user_parameters>
-<p_t></p_t>
-</parameters>
-</action>
-HERE
 end
