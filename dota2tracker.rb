28ae8e5712b1e234e8e2b491094e34e05014f8a9
github@yeahandstuff.net <github@yeahandstuff.net>
2014-03-01 16:25:00 -0500

    Trying to get things to work...

--- dota2tracker.rb
+++ dota2tracker.rb
@@ -1,12 +1,14 @@
 require 'sinatra'
 require './models/last_match_helper'
 
-STEAM_API_KEY = 
+set :environment, :production
+
+# STEAM_API_KEY = 
 get '/' do
   account_id = params['accountid']
   steam_key = params['api_key']
   return "Specify a steam api key(api_key) and 32 bit id(account_id)!" unless account_id && steam_key
   lmh = LastMatchHelper.new(steam_key, account_id.to_i )
-  require 'debugger'; debugger
-  lmh.get_some_interesting_information
+  #require 'debugger'; debugger
+  #lmh.get_some_interesting_information
 end
